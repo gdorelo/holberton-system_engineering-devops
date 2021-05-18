3c701e08ca07b37005915d5e013f2537eabce55c
Gianluca <gianlucadorelo@gmail.com>
2021-05-18 13:47:13 -0300

    Fixed tasks and added adv task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' Write a function that queries the Reddit API  '''
+
+
+def number_of_subscribers(subreddit):
+    ''' Returns subscriber count of subreddit or 0 '''
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'user-agent': 'app/0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
+    return 0
